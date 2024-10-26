4d196d01290cecbb7e409b739ea1099e258dbc1a
James Gyampoh <jamesgyampoh560@gmail.com>
2024-10-25 23:36:22 -0400

    flutter app

--- lib/shared/errors/failure.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-class Failure {
-  String message;
-  Failure(this.message);
-
-  @override
-  String toString() => 'Failure(message: $message)';
-}
